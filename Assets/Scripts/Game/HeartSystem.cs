0abc2a173fd5d7cbcb46f93b88586ffe5b942e6f
thommy000 <thomasvictorf10@gmail.com>
2023-06-18 15:08:13 -0300

    life system incomplete

---
+++ Assets/Scripts/Game/HeartSystem.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartSystem : MonoBehaviour
+{
+    public int life;
+    puplic item maxLife;
+
+    public Image[] heart;
+    public Sprite cheio;
+    public Sprite vazio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
