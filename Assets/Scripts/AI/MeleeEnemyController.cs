208691206dae17ee254c09ed75c9a18d94f1e04d
marcos-arthur <marcos.marcos.arthur291@gmail.com>
2023-06-19 16:18:05 -0300

    build

--- Assets/Scripts/AI/MeleeEnemyController.cs
+++ Assets/Scripts/AI/MeleeEnemyController.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.PlayerSettings;
-using static UnityEngine.GraphicsBuffer;
 
 [RequireComponent(typeof(EnemyController))]
 public class MeleeEnemyController : MonoBehaviour
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditorInternal.Profiling.Memory.Experimental;
 using UnityEngine;
 using UnityEngine.UI;
 
